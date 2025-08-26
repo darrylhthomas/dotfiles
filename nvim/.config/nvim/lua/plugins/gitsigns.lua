2d47438a2d578ddad45afd6a5b768427801d4cfa
Darryl H. Thomas <dhthomas@apple.com>
2025-08-25 23:26:06 -0700

    added gitsigns to nvim

---
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,14 @@
+return {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+        require("gitsigns").setup()
+
+        vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { desc = "Preview git hunk" })
+        vim.keymap.set(
+            "n",
+            "<leader>gb",
+            ":Gitsigns toggle_current_line_blame<CR>",
+            { desc = "Toggle current line git blame" }
+        )
+    end,
+}
