80a83b53e47fb233cc43a17eceb9ddf7805a36cc
Darryl H. Thomas <darrylhthomas@me.com>
2025-08-29 09:13:52 -0700

    add nvim-tree

--- nvim/.config/nvim/lua/plugins/auto-session.lua
+++ nvim/.config/nvim/lua/plugins/auto-session.lua
@@ -2,7 +2,23 @@ return {
     "rmagatti/auto-session",
     config = function()
         local auto_session = require("auto-session")
+        local function close_nvim_tree()
+            require("nvim-tree.view").close()
+        end
+
+        local function open_nvim_tree()
+            require("nvim-tree.view").open()
+        end
+
         auto_session.setup({
+            log_level = "error",
+            pre_save_cmds = { close_nvim_tree },
+            post_save_cmds = { open_nvim_tree },
+            post_open_cmds = { open_nvim_tree },
+            post_restore_cmds = { open_nvim_tree },
+            cwd_change_handling = {
+                restore_upcoming_session = true,
+            },
             auto_save = true,
             suppressed_dirs = { "~/", "~/Downloads", "~/Documents", "~/Desktop", "~/src" },
         })
